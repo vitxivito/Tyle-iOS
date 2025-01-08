c94880728a0e397c7e9e16f6777beaf5fc728caf
vitxivito <133760252+vitxivito@users.noreply.github.com>
2025-01-08 18:24:57 +0100

    Initial Commit

---
+++ Tyle/TyleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TyleApp.swift
+//  Tyle
+//
+//  Created by Vitoria Garcia on 8/1/25.
+//
+
+import SwiftUI
+
+@main
+struct TyleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
