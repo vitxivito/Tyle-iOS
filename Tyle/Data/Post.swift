7038fb8de904c2c73f33397fd8be2e9b22c54a0b
vitxivito <133760252+vitxivito@users.noreply.github.com>
2025-01-27 14:26:26 +0100

    Create Post.swift

---
+++ Tyle/Data/Post.swift
@@ -0,0 +1,15 @@
+//
+//  Post.swift
+//  Tyle
+//
+//  Created by Vitoria Garcia on 27/1/25.
+//
+
+import Foundation
+
+struct Post {
+    let caption: String
+    let date: Date
+    let img: String
+    let userId: String
+}
