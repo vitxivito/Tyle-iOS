47b12706243918b85410070ce5d55e2829b22221
vitxivito <133760252+vitxivito@users.noreply.github.com>
2025-05-14 18:05:21 +0200

    WipProfile

--- Tyle/Presentation/Profile/ProfileView.swift
+++ Tyle/Presentation/Profile/ProfileView.swift
@@ -10,7 +10,38 @@ import SwiftUI
 struct ProfileView: View {
     @StateObject var vm = ProfileViewModel()
     var body: some View {
-        Text("Profile")
+        VStack{
+            Text("user-id")
+            HStack(spacing: 32){
+                Button{
+                    
+                } label: {
+                    Image(._03)
+                        .resizable()
+                        .aspectRatio(1, contentMode: .fit)
+                        .frame(maxWidth: 100)
+                        .clipShape(.buttonBorder)
+                }
+                Button {
+                } label: {
+                    VStack {
+                        Text("0")
+                        Text("Amigos")
+                    }
+                }
+                .tint(.black)
+                VStack {
+                    Text("1")
+                    Text("Posts")
+                }
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            Divider()
+            Text("biografia")
+                .frame(maxWidth: .infinity, alignment: .leading)
+            Spacer()
+        }
+        .padding(.horizontal)
     }
 }
 
